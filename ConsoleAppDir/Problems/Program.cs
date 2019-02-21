d20104a162ca83ae6ba4597a236fd810d397f03e
Sinawo Mngxuma <g16M4204@ict.ru.ac.za>
2019-02-21 16:29:39 +0200

    second commit

---
+++ ConsoleAppDir/Problems/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Problems
+{
+   public class Program
+    {
+        string Prifix(string input)
+        {
+                 int CountChars =0;
+
+                 foreach(char s in input)CountChars++;
+                 string[] found = input.Split(" ");
+                 int words = found.Length;
+
+                 
+          return Convert.ToString(CountChars) + "," + Convert.ToString(words) +":"+input;
+
+                
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
